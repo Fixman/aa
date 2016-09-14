c5b17fb33f4eb954fd7cd8510aa013f9a8f15ec8
Martin Fixman <martinfixman@gmail.com>
2016-09-14 11:55:09 -0300

    Woops.

--- train_test_split.sh
+++ train_test_split.sh
@@ -7,5 +7,7 @@ cat $1 | sed -E 's/([^\\]",|\^[|\]$)\s*/\1\n/g' > $split
 lines=$(wc -l $split | cut -f1 -d' ')
 test_lines=$((lines / 10))
 
-( head -n -$test_lines $split | sed '$s/,//' ; echo ']' ) > $(sed -E 's/_\w+\./_train./' <<< $1)
+( head -n -$test_lines $split | sed '$s/,\s*$//' ; echo ']' ) > $(sed -E 's/_\w+\./_train./' <<< $1)
 ( echo '['; tail -n $test_lines $split ) > $(sed -E 's/_\w+\./_test./' <<< $1)
+
+rm -f $split
