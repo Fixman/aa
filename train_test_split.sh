d8d273235a66dd1bd861bba84fc1f36560a1eddc
Martin Fixman <martinfixman@gmail.com>
2016-09-18 15:36:36 -0300

    Some changes.

--- train_test_split.sh
+++ train_test_split.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 set -e
+set -x
 
 split=$(mktemp)
-cat $1 | sed -E 's/([^\\]",|\^[|\]$)\s*/\1\n/g' > $split
+cat $1 | sed -E 's/([^\\]",|^\[|\]$)\s*/\1\n/g' > $split
 
-lines=$(wc -l $split | cut -f1 -d' ')
+lines=$(wc -l $split | sed -E 's/^\s*([0-9]+).*/\1/')
 test_lines=$((lines / 10))
 
 ( head -n -$test_lines $split | sed '$s/,\s*$//' ; echo ']' ) > $(sed -E 's/_\w+\./_train./' <<< $1)
 ( echo '['; tail -n $test_lines $split ) > $(sed -E 's/_\w+\./_test./' <<< $1)
 
-rm -f $split
+# rm -f $split
