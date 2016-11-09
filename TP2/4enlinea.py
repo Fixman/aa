08554d7e0069d6e95892e4c38a6f299cef1e1737
Martin Fixman <martinfixman@gmail.com>
2016-11-09 14:28:01 -0300

    Clase ConnectFour.

--- TP2/4enlinea.py
+++ TP2/4enlinea.py
@@ -21,20 +21,84 @@ class QLearningPlayer(object):
         raise NotImplementedError
 
 class ConnectFour(object):
-    rows = 6
-    cols = 7
-    moves = cols
+    moves = 7
 
-    class Point(Enum):
-        empty = 0
-        red = 1
-        white = 2
+    class Board(object):
+        rows = 6
+        cols = 7
 
-    def __init__(self, red, white, rows = 6, cols = 7):
-        self.board = [[self.Point.empty for x in range(cols)] for y in range(rows)]
+        class Point(Enum):
+            empty = 0
+            red = 1
+            white = 2
+
+        class WinnerState(Enum):
+            null = 0
+            red = 1
+            white = 2
+            tie = 3
+
+        def __init__(self):
+            self.state = [[self.Point.empty for x in range(self.cols)] for y in range(self.rows)]
+
+        def put(self, color, move):
+            self.board[max(x for x in board if board[x] == Point.empty)] = color
+        
+        def check_position(self, y, x):
+            deltas = [
+                (0, 1),
+                (1, 0),
+                (1, 1),
+                (1, -1),
+            ]
+
+            if self.state[y][x] == self.Point.empty:
+                return None
+
+            color = self.state[y][x]
+            for dy, dx in deltas:
+                for m in range(3):
+                    try:
+                        if self.state[y + dy * m][x + dx * m] != color:
+                            return None
+                    except IndexError:
+                        break
+
+            return color
+
+        def winner(self):
+            available = False
+            for i in range(self.rows):
+                for j in range(self.cols):
+                    if self.state[i][j] == self.Point.empty:
+                        available = True
+                    else:
+                        p = self.check_position(i, j)
+                        if p:
+                            return p
+
+            if not available:
+                return self.WinnerState.tie
+
+            return self.WinnerState.null
+
+    def __init__(self, red, white):
+        self.board = self.Board()
+        red.color = self.Board.Point.red
+        white.color = self.Board.Point.white
+
+        self.red = red
+        self.white = white
 
     def play(self):
-        raise NotImplementedError
+        current, opponent = self.red, self.white
+        print(self.board.winner())
+        while self.board.winner() == self.Board.WinnerState.null:
+            move = current.move(self.board)
+            self.board.put(current.color, move)
+
+            current, opponent = opponent, current
+        return self.board.winner()
 
 def parse_args():
     parser = ArgumentParser(description = 'Juega al 4 en linea entre dos jugadores con Q-learning.')
