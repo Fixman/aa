d030b1fe0277eb70cce19aecbe504255eb3597d1
Martin Fixman <martinfixman@gmail.com>
2016-11-09 19:18:25 -0300

    Comentarios y bug corregido con la eleccion del mejor movimiendo en Q-learners.

--- TP2/connectfour.py
+++ TP2/connectfour.py
@@ -6,28 +6,42 @@ class Slot(Enum):
     red = 1
     blue = 2
 
+# El estado de un tablero. Puede no estar ganando nadie, que este ganando
+# algun jugador, o que haya un empate.
 class WinnerState(Enum):
     null = 0
     red = 1
     blue = 2
     tie = 3
 
+# Un tablero que empieza vacio, y se le puede agregar una ficha
+# en alguna ranura.
 class Board(object):
     rows = 6
     cols = 7
 
+    # El estado inicial es con todos los slots vacios.
     def __init__(self):
         self.state = [[Slot.empty for x in range(self.cols)] for y in range(self.rows)]
 
+    # Dar una lista de movimienos validos, que tengan lugar para poner una ficha.
     def available_moves(self):
         return [x for x in range(self.cols) if self.col(x)[0] == Slot.empty]
 
+    # Poner una ficha de algun color en alguna ranura.
     def put(self, color, move):
-        self.state[max(x for x in range(self.rows) if self.state[x][move] == Slot.empty)][move] = color
+        try:
+            self.state[max(x for x in range(self.rows) if self.state[x][move] == Slot.empty)][move] = color
+        except ValueError:
+            import ipdb
+            ipdb.set_trace()
 
+    # Devolver una copia de una columna.
     def col(self, c):
         return [self.state[x][c] for x in range(self.rows)]
     
+    # Devolver si hay un cuatro-en-linea de algun jugador
+    # empezando en el punto (y, x)
     def check_position(self, y, x):
         deltas = [
             (0, 1),
@@ -52,6 +66,7 @@ class Board(object):
 
         return None
 
+    # Devolver quien esta ganando.
     def winner(self):
         available = False
         for i in range(self.rows):
@@ -68,6 +83,7 @@ class Board(object):
 
         return WinnerState.null
 
+    # Imprimir el tablero de una manera linea y colorida.
     def pretty_print(self):
         colors = {
             # Slot.empty: '\033[1;37mo',
@@ -77,10 +93,11 @@ class Board(object):
         }
         return '\n'.join(' '.join(colors[p] for p in q) for q in self.state)
 
-
+# Un 4 en linea. Tiene un tablero y dos jugadores.
 class ConnectFour(object):
     moves = range(7)
 
+    # Inicializar el juego con los dos jugadores.
     def __init__(self, red, blue):
         self.board = Board()
         red.color = Slot.red
@@ -89,6 +106,7 @@ class ConnectFour(object):
         self.red = red
         self.blue = blue
 
+    # Jugar al 4 en linea hasta que alguien gane o haya un empate.
     def play(self):
         current, opponent = self.red, self.blue
         while self.board.winner() == WinnerState.null:
--- TP2/players.py
+++ TP2/players.py
@@ -4,40 +4,55 @@ from collections import defaultdict
 
 from connectfour import Slot
 
+# Elegir el argmax de una lista.
+# Si hay varios valores iguales, elegir uno al azar.
 def randargmax(a):
     return random.choice([i for i, x in enumerate(a) if x == max(a)])
 
+# Jugador que hace Q Learning.
 class QLearningPlayer(object):
+    # Inicializar jugador.
+    # moves: lista de jugadas posibles.
+    # epsilon: posibilidad de que el jugador haga un movimiento al azar.
+    # alpha, gamma: valores de la ecuacion del Q-learning.
+    # initial: valor inicial de los Q sin predicciones.
     def __init__(self, moves, epsilon = .2, alpha = .3, gamma = .9, initial = 1.):
         self.Q = defaultdict(lambda: initial)
         self.epsilon = epsilon
         self.alpha = alpha
         self.gamma = gamma
 
+    # Elegir el mejor movimiento, y anotarlo como ultimo movimiento.
     def move(self, board):
         move = self.best_move(board)
         self.last_move = move
-        print(move)
         return move
 
+    # Elegir el mejor movimiento.
+    # Con probabilidad epsilon, hacer un movimiento al azar.
+    # Si no, elegir algun valor que tenga Q maximal.
     def best_move(self, board):
         moves = board.available_moves()
         if random.random() < self.epsilon:
             return random.choice(moves)
 
-        # print(self.Q)
-        return randargmax([self.Q[x] for x in moves])
+        m = randargmax([self.Q[x] for x in moves])
+        return moves[m]
 
+    # Anotar el reward de una jugada.
+    # Aca deberia estar la ecuacion Q.
     def reward(self, board, value):
         raise NotImplementedError
 
+# Jugador que hace jugadas al azar.
 class RandomPlayer(object):
     def __init__(self, moves):
         self.moves = moves
 
+    # Elegir un movimiento valido al azar.
     def move(self, board):
-        vals = [x for x in self.moves if board.col(x)[0] == Slot.empty]
-        return random.choice(vals)
+        return random.choice(board.available_moves())
 
+    # No nos importa el reward.
     def reward(self, board, value):
         pass
